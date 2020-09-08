6254368ab40c4e8fd7baa705d8ba99f1f6063d7f
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-09-07 23:49:12 -0400

    Create module for Objective-C

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@
 
 // This crate is only available for 32 and 64 bit systems.
 #![cfg(any(target_pointer_width = "32", target_pointer_width = "64"))]
+
+pub mod objc;
---
+++ src/objc/mod.rs
@@ -0,0 +1,4 @@
+//! [Objective-C](https://developer.apple.com/documentation/objectivec) library.
+
+#[link(name = "objc", kind = "dylib")]
+extern "C" {}
