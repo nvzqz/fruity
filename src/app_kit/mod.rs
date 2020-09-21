b801dc4fab3bc65f6b2f09817f680ebeeb42f4ee
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-09-21 00:18:26 -0400

    Change app_kit cfg to not get auto-formatted
    Rustfmt de-indents comments.

--- src/app_kit/mod.rs
+++ src/app_kit/mod.rs
@@ -5,13 +5,10 @@
 //! This module corresponds to the **`app_kit`**
 //! [feature flag](../index.html#feature-flags).
 
+// `mac_catalyst` is enabled by `build.rs` for `x86_64-apple-ios-macabi`.
 #![cfg(all(
     feature = "app_kit",
-    any(
-        target_os = "macos",
-        // Enabled by `build.rs` for `x86_64-apple-ios-macabi`.
-        mac_catalyst,
-    )
+    any(target_os = "macos", mac_catalyst),
 ))]
 
 mod version;
