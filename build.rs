f74176acf081796af9271134dca40ba6141536c1
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-12-07 00:32:31 -0500

    Fix env var read by `build.rs`

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 fn main() {
-    if let Ok(target_env) = env::var("TARGET_ENV") {
+    if let Ok(target_env) = env::var("TARGET") {
         match target_env.as_str() {
             "x86_64-apple-ios-macabi" => {
                 println!("cargo:rustc-cfg=mac_catalyst");
