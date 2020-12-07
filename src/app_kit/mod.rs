174554efd9d5b6956ea5d41b17915e92e32966fd
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-12-07 01:34:24 -0500

    Flatten `cfg`s in `app_kit` and `ui_kit`
    This is the style preferred by `cargo fmt`.

--- src/app_kit/mod.rs
+++ src/app_kit/mod.rs
@@ -6,10 +6,7 @@
 //! [feature flag](../index.html#feature-flags).
 
 // `mac_catalyst` is enabled by `build.rs` for `x86_64-apple-ios-macabi`.
-#![cfg(all(
-    feature = "app_kit",
-    any(target_os = "macos", mac_catalyst),
-))]
+#![cfg(all(feature = "app_kit", any(target_os = "macos", mac_catalyst)))]
 
 mod version;
 
