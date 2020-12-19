7f018ddfa87fa39be6821f11bb5e5dff73bc8b53
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-12-19 15:03:21 -0500

    Create `objc::Imp` type alias

--- src/objc/method/mod.rs
+++ src/objc/method/mod.rs
@@ -1,3 +1,13 @@
 mod description;
 
 pub use description::*;
+
+/// A pointer to the function of a method implementation.
+///
+/// The first argument is a pointer to `self` (that is, the memory for the
+/// particular instance of this class, or, for a class method, a pointer to the
+/// metaclass). The second argument is the method selector. The method arguments
+/// follow.
+///
+/// See [documentation](https://developer.apple.com/documentation/objectivec/objective-c_runtime/imp?language=objc).
+pub type Imp = unsafe extern "C" fn();
