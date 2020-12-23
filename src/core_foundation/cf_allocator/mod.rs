42bef45163ff67006e23327f0a1ad5460ac7b84f
Nikolai Vazquez <hello@nikolaivazquez.com>
2020-12-23 11:42:19 -0500

    Add methods to `CFAllocator`

--- src/core_foundation/cf_allocator/mod.rs
+++ src/core_foundation/cf_allocator/mod.rs
@@ -1,4 +1,6 @@
-use super::CFType;
+use super::{CFType, CFTypeID};
+use crate::{core::Arc, core_foundation::sys};
+use std::{mem::MaybeUninit, ptr};
 
 mod callbacks;
 mod context;
@@ -93,5 +95,102 @@ impl CFAllocator {
             static kCFAllocatorUseContext: &'static CFAllocator;
         }
         kCFAllocatorUseContext
+    }
+}
+
+impl CFAllocator {
+    /// Returns the type identifier for `CFAllocator`.
+    ///
+    /// See [documentation](https://developer.apple.com/documentation/corefoundation/1521328-cfallocatorgettypeid?language=objc).
+    #[inline]
+    #[doc(alias = "CFAllocatorGetTypeID")]
+    pub fn type_id() -> CFTypeID {
+        unsafe { sys::CFAllocatorGetTypeID() }
+    }
+
+    /// Creates an allocator object.
+    ///
+    /// See [documentation](https://developer.apple.com/documentation/corefoundation/1521159-cfallocatorcreate?language=objc).
+    #[inline]
+    #[doc(alias = "CFAllocatorCreate")]
+    pub unsafe fn create(
+        allocator: Option<&CFAllocator>,
+        mut context: CFAllocatorContext,
+    ) -> Arc<Self> {
+        let allocator: *const CFAllocator = match allocator {
+            Some(allocator) => allocator,
+            None => ptr::null(),
+        };
+        Arc::from_raw(sys::CFAllocatorCreate(allocator, &mut context))
+    }
+
+    /// Returns the context of the default allocator.
+    ///
+    /// See [documentation](https://developer.apple.com/documentation/corefoundation/1521267-cfallocatorgetcontext?language=objc).
+    #[inline]
+    #[doc(alias = "CFAllocatorGetContext")]
+    pub fn default_context() -> CFAllocatorContext {
+        unsafe {
+            let mut context = MaybeUninit::uninit();
+            sys::CFAllocatorGetContext(ptr::null(), context.as_mut_ptr());
+            context.assume_init()
+        }
+    }
+
+    /// Returns the context of this allocator.
+    ///
+    /// See [documentation](https://developer.apple.com/documentation/corefoundation/1521267-cfallocatorgetcontext?language=objc).
+    #[inline]
+    #[doc(alias = "CFAllocatorGetContext")]
+    pub fn context(&self) -> CFAllocatorContext {
+        unsafe {
+            let mut context = MaybeUninit::uninit();
+            sys::CFAllocatorGetContext(self, context.as_mut_ptr());
+            context.assume_init()
+        }
+    }
+}
+
+/// Getting and setting the default allocator.
+impl CFAllocator {
+    /// Gets the default allocator object for the current thread and retains it.
+    ///
+    /// See [documentation](https://developer.apple.com/documentation/corefoundation/1521325-cfallocatorgetdefault?language=objc).
+    #[inline]
+    #[doc(alias = "CFAllocatorGetDefault")]
+    pub fn default() -> Arc<Self> {
+        unsafe { Arc::retain_raw(sys::CFAllocatorGetDefault()) }
+    }
+
+    /// Gets the default allocator object for the current thread without
+    /// retaining it.
+    ///
+    /// See [documentation](https://developer.apple.com/documentation/corefoundation/1521325-cfallocatorgetdefault?language=objc).
+    ///
+    /// # Safety
+    ///
+    /// Ownership of the allocator returned by `CFAllocatorGetDefault` follows
+    /// [The Get Rule](https://developer.apple.com/library/archive/documentation/CoreFoundation/Conceptual/CFMemoryMgmt/Concepts/Ownership.html#//apple_ref/doc/uid/20001148-SW1),
+    /// so you do not own it and cannot be certain of the object's life span.
+    #[inline]
+    #[doc(alias = "CFAllocatorGetDefault")]
+    pub unsafe fn default_unretained<'a>() -> &'a Self {
+        &*sys::CFAllocatorGetDefault()
+    }
+
+    /// Sets this given allocator as the default for the current thread.
+    ///
+    /// See [documentation](https://developer.apple.com/documentation/corefoundation/1521325-cfallocatorgetdefault?language=objc).
+    ///
+    /// # Safety
+    ///
+    /// This allocator must be able to deal with arbitrary allocation requests.
+    ///
+    /// This allocator must never be released, even if another allocator
+    /// replaces it as the default.
+    #[inline]
+    #[doc(alias = "CFAllocatorSetDefault")]
+    pub unsafe fn make_default(&self) {
+        sys::CFAllocatorSetDefault(self);
     }
 }
--- src/core_foundation/sys.rs
+++ src/core_foundation/sys.rs
@@ -1,6 +1,9 @@
 //! Raw unsafe C functions exposed by `CoreFoundation.framework`.
 
-use super::{Boolean, CFHashCode, CFIndex, CFType, CFTypeID};
+use super::{
+    Boolean, CFAllocator, CFAllocatorContext, CFHashCode, CFIndex, CFOptionFlags, CFType, CFTypeID,
+};
+use std::ffi::c_void;
 
 #[allow(missing_docs)]
 #[link(name = "CoreFoundation", kind = "framework")]
@@ -13,4 +16,37 @@ extern "C" {
 
     pub fn CFHash(cf: *const CFType) -> CFHashCode;
     pub fn CFGetTypeID(cf: *const CFType) -> CFTypeID;
+
+    pub fn CFAllocatorGetTypeID() -> CFTypeID;
+
+    pub fn CFAllocatorCreate(
+        allocator: *const CFAllocator,
+        context: *mut CFAllocatorContext,
+    ) -> *mut CFAllocator;
+
+    pub fn CFAllocatorAllocate(
+        allocator: *const CFAllocator,
+        size: CFIndex,
+        hint: CFOptionFlags,
+    ) -> *mut c_void;
+
+    pub fn CFAllocatorReallocate(
+        allocator: *const CFAllocator,
+        ptr: *mut c_void,
+        new_size: CFIndex,
+        hint: CFOptionFlags,
+    ) -> *mut c_void;
+
+    pub fn CFAllocatorDeallocate(allocator: *const CFAllocator, ptr: *mut c_void);
+
+    pub fn CFAllocatorGetPreferredSizeForSize(
+        allocator: *const CFAllocator,
+        size: CFIndex,
+        hint: CFOptionFlags,
+    ) -> CFIndex;
+
+    pub fn CFAllocatorGetDefault() -> *const CFAllocator;
+    pub fn CFAllocatorSetDefault(allocator: *const CFAllocator);
+
+    pub fn CFAllocatorGetContext(allocator: *const CFAllocator, context: *mut CFAllocatorContext);
 }
